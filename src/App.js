5bd5074b3ff3400112ab5398a357c67272893e70
Joe Bay <jbay55278@gmail.com>
2020-08-03 13:08:32 -0800

    got the todo to function normally, just need to add functionality to the clear completed button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,55 @@
 import React from 'react';
+import TodoList from './components/TodoList'
+import TodoForm from './components/TodoForm'
+import "./components/Todo.css"
 
+
+const initialTodo = [{}]
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super()
+    this.state = {
+      todos: initialTodo,
+    }
+  }
+
+  toggleItem = id => {
+    // use arrow method
+    this.setState({
+      todos: this.state.todos.map(item => {
+        if (item.id === id) {
+          return {
+            ...item,
+            completed: !item.completed
+          }
+        }else {
+          return item;
+        }
+      })
+    })
+  }
+  addTodo = todoName => {
+    const newTodo = {
+      task: todoName,
+      id: new Date(),
+      completed: false
+    }
+    this.setState({
+      todos: [...this.state.todos, newTodo]
+    })
+  }
+
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <div className="header">
+          <h2>Welcome to your Todo App!</h2>
+          <TodoForm addItem={this.addTodo} />
+        </div>
+        <TodoList toggleItem={this.toggleItem} todos={this.state.todos}/>
       </div>
     );
   }
